8bc9d83e989a54072d4a1107ee9976b58406c5c8
Tim Fawcett <timfawcett21@gmail.com>
2022-04-11 19:36:13 +1000

    added grandparent model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_09_062100) do
+ActiveRecord::Schema.define(version: 2022_04_11_093203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "grandparents", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "availability"
+    t.string "interest"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
