2b6b16f1ea4825d3797e816e96ace0bf2a27b064
Khadija Bulhan <kbulhan8@gmail.com>
2022-04-12 20:31:21 +1000

    booking model adjusted

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :grandparent
 
   validates :name, :description, :location, :interest, :availability, presence: true
-  validates :grandparent_id, presence: true
-  validates :user_id, presence: true
-end
+  # validates :grandparent_id, presence: true
+  # validates :user_id, presence: true
+ end
