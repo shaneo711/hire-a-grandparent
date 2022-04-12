8f8aa86fadfe25d20b2a4f2715f1fce87cb7b3f5
Shane Garland <shane.garland1@gmail.com>
2022-04-12 19:54:07 +1000

    changes routes to use "resources" helper, also created basic navbar"

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get "grandparents", to: "grandparents#index"
-  # make sure "new" route is above "show" route when we get to that.
-  get "grandparents/new", to: "grandparents#new"
-  post "grandparents", to: "grandparents#create"
-
+  resources :grandparents, only: %i[index show new create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
