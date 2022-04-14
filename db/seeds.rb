8e0ff4feb28be589e3787b3fecba933e0893bd49
Shane Garland <shane.garland1@gmail.com>
2022-04-14 19:32:02 +1000

    seed creates fake user and 10 grandparents

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,18 +4,38 @@
 # Examples:
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 require 'faker'
-
+puts "Wiping database..."
+Grandparent.destroy_all
+User.destroy_all
+puts "Database wiped!"
+puts "---------------"
 puts "Seeding..."
+puts "---------------"
+puts "Creating fake user..."
+user = User.new
+user.email = 'admin@lewagon.com'
+user.password = 'password'
+user.save!
+puts "---------------"
+puts "User Details: "
+puts "Email: #{user.email}"
+puts "Password: #{user.password}"
+puts "---------------"
+
+puts "Creating grandparents..."
 
 10.times do
-Grandparent.create(
-  name: Faker::Name.name,
-  location: Faker::Address.city,
-  interest: Faker::Hobby.activity,
-  description: Faker::Quote.famous_last_words,
-  availability: Faker::Date.between(from: 2.days.ago, to: Date.today),
-)
+  grandparent = Grandparent.new(
+    name: Faker::Name.name,
+    location: Faker::Address.city,
+    interest: Faker::Hobby.activity,
+    description: Faker::Quote.famous_last_words,
+    availability: Faker::Date.between(from: 2.days.ago, to: Date.today),
+    user_id: user.id
+  )
+  grandparent.save!
+  puts "Grandparent #{grandparent.id} created!"
 end
-
+puts "---------------"
+puts "Seed complete!"
