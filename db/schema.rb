dcde3aa571363ab4ac9f0cd29c82eb9fd0fd0038
Shane Garland <shane.garland1@gmail.com>
2022-04-14 20:29:53 +1000

    created index page with cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_082519) do
+ActiveRecord::Schema.define(version: 2022_04_14_100832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_04_13_082519) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_grandparents_on_user_id"
   end
 
