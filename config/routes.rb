0971643fa9fd3f359dcaa04fb68e5dc0fdc45f47
Shane Garland <shane.garland1@gmail.com>
2022-04-15 10:46:08 +1000

    added redirect to user dashboard upon succesful sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  # Route to redirect user to dashboard upon successful sign in.
+  get '/user' => "pages#dashboard", :as => :user_root
+
   resources :grandparents, only: %i[index show new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
