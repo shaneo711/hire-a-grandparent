04d4eff81d5f23a29f6f934d951edc5b710ae575
Tim Fawcett <timfawcett21@gmail.com>
2022-04-16 14:14:18 +1000

    minor front end adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/user' => "pages#dashboard", :as => :user_root
 
   resources :grandparents, only: %i[index show new create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
