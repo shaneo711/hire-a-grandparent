d4371a94df80f3777199d44ff81da97f4d2c02b7
Shane Garland <shane.garland1@gmail.com>
2022-04-21 20:17:17 +1000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'reviews/update'
-  get 'reviews/edit'
-  get 'reviews/destroy'
-  get 'reviews/index'
-  get 'reviews/show'
   devise_for :users
   root to: 'pages#home'
 
@@ -15,7 +8,9 @@ Rails.application.routes.draw do
 
   resources :grandparents do
     resources :bookings, only: %i[new create edit update index show]
+    resources :reviews, only: %i[new create index]
   end
+  
 
   resources :bookings, only: %i[destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
