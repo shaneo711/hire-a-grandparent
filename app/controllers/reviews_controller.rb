d6aa198521346cc5d69c14e55d7fc01bfc4e3a70
Khadija Bulhan <kbulhan8@gmail.com>
2022-04-21 20:03:00 +1000

    reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,34 +15,9 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # def update
-  #    @review = review.find(params[:id])
-  #  if @review.update_attributes(review_param)
-  #       redirect_to :action => 'show', :id => @review
-  #  else
-  #    @booking = Booking.all
-  #    render :action => 'edit'
-  #   end
-  # end
-
-  def edit
-    @review = Review.find(review_params)
-    #do we want users to go back and edit reviews??
-  end
-
-  def destroy #do we want users to delete reviews?
-    @review.destroy
-    redirect_to review_path
-  end
-
   def index
     @reviews = Review.all
   end
-
-  # def show
-  # #   @review = review.find(params[:id])
-   #dont need show page unless paragraphs of informatiom is added
-  # end
   
   private
   
