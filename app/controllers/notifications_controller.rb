3143386addab1c3434b60bbdfe35a38756540aab
Tim Fawcett <timfawcett21@gmail.com>
2022-04-23 12:10:35 +1000

    loop and conditionals

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,10 +2,15 @@ class NotificationsController < ApplicationController
   skip_after_action :verify_policy_scoped
 
   def index
+    # def unchecked_notifications
+    #   notifications.where("created_at > ?", last_checked_notifications)
+    # end
     @notifications = current_user.unchecked_notifications
     render
     Thread.new do
       current_user.update(last_checked_notifications: DateTime.now)
     end
   end
+
+
 end
